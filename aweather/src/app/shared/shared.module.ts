1d39ef95c0c7961f82499a21bede148fa0f4a222
unknown <agabrieladuran1@gmail.com>
2018-12-19 15:52:05 -0400

    directivas

--- aweather/src/app/shared/shared.module.ts
+++ aweather/src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MaterialFormsModule} from './material-forms/material-forms.module';
+import { WeatherIconComponent } from './weather-icon/weather-icon.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [WeatherIconComponent],
   imports: [
     CommonModule,
     MaterialFormsModule
   ],
-  exports: [MaterialFormsModule]
+  exports: [MaterialFormsModule, WeatherIconComponent]
 })
 export class SharedModule {}
--- aweather/src/app/weather/weather-card/weather-card.component.ts
+++ aweather/src/app/weather/weather-card/weather-card.component.ts
@@ -22,13 +22,13 @@ export class WeatherCardComponent implements OnInit {
       this.weather.city = 'Madrid ES';
       this.weather.date = 'Tue, 01 May 2018 06:00 PM CEST';
       this.weather.humidity = 32;
-      this.weather.info = 'Nublado';
+      this.weather.info = 'Cloudy';
       this.weather.maxtemperature = 24;
       this.weather.mintemperature = 10;
       this.weather.pressure = 1080;
       this.weather.sunrise = '6:58 am';
       this.weather.sunset = '11:40 pm';
-      this.weather.temperature = 18;
+      this.weather.temperature = 2;
       this.weather.winddirection = 180;
       this.weather.windspeed = 26;
       this.weather.forecast = new Forecast();
@@ -42,12 +42,26 @@ export class WeatherCardComponent implements OnInit {
 
     }, 1000);
 
-    setTimeout(() => {
+    /*setTimeout(() => {
       this.weather.temperature = 20;
-    }, 3000);
+    }, 3000);*/
+  }
+    // tslint:disable-next-line:comment-format
+    // modificar el color de la temperatura depediendo de si es muy alta o muy baja//
 
-    console.log(this.weather);
+    public getColorTemperature(): string {
+      if (this.weather !== undefined && this.weather.temperature >= 24) {
+        return '#EF6C00';
+      }
+
+      if (this.weather !== undefined && this.weather.temperature <= 10) {
+        return '#0277BD';
+      }
+
+      return '#212121';
+    }
+    // console.log(this.weather);
   }
 
 
-}
+
